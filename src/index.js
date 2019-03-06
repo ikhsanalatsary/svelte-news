5b7faf2e916528be15b41f0041d13c21baf95673
Abdul Fattah Ikhsan <skull.saders18@gmail.com>
2019-03-06 10:00:18 -0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import "semantic-ui/dist/semantic.css";
+import HelloWorld from "./HelloWorld.html";
+// import "semantic-ui-less/semantic.less";
+
+const app = new HelloWorld({
+  target: document.getElementById("root"),
+  data: {
+    name: "Svelte",
+    articles: undefined,
+    search: false
+  }
+});
