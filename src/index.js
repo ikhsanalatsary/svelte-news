e88d88fc0e1e3d00b8e14d2414e1022a68c1306c
ikhsanalatsary <ikhsannetwork@gmail.com>
2019-03-17 01:31:22 +0700

    infinite scroll

--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@
 import HelloWorld from "./HelloWorld.html";
 // import "semantic-ui-less/semantic.less";
 
+let window_updating = false;
 const app = new HelloWorld({
   target: document.getElementById("root"),
   data: {
     name: "Svelte",
     articles: undefined,
-    search: false
+    search: undefined,
+    count: 0,
+    currentPage: 1,
+    fetching: false,
+    y: 0,
+    isSearch: false
   }
 });
 
